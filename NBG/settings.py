2cecc0c23299554c7e59cab45206d69bf0f0c1ae
JustMe2-wq <goylew13@gmail.com>
2025-07-28 20:17:10 -0500

    message

--- NBG/settings.py
+++ NBG/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('Wieselew25!')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if not 'ON_HEROKU' in os.environ:
+        DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -98,9 +99,6 @@ else:
         'PASSWORD': 'admin',
         }
     }
-    
-    if not 'ON_HEROKU' in os.environ:
-        DEBUG = True
 
 
 
